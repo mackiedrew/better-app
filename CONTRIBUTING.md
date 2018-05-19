f8abc1de2b412464633cb80578df2f56080a4e1c
Mackie Drew <mackiedrew@gmail.com>
2018-05-18 22:27:57 -0400

    :sparkles: Added commit formatting script

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -15,3 +15,42 @@ To work on the App on both Android and iOS you'll need I'm afraid you'll need OS
 ```bash
 brew install yarn
 ```
+
+## Commit Messages
+
+Commits should be as small as possible, atomic commits are highly recommended. Commits do not need to be feature-complete but should all be operational. If work-in-progress (WIP) commits are created they should be squashed or renamed with an interactive rebase.
+
+### Title
+
+1. Title must be less than 50 characters. Not including Gitmoji.
+2. Title needs at least one valid [Gitmoji](https://gitmoji.carloscuesta.me).
+3. Title needs a space after all Gitmoji.
+4. Title must begin with a capital letter.
+5. Title must not have a period at the end.
+
+### Message
+
+Should include relevant details, and give all required context. Ideally if referencing an issue, should include an issue number as well.
+
+## Branches
+
+The main branches are as follows:
+
+- *development*: This is the main branch, normally called `master`.
+- *production*: This branch reflects what is currently in production at any one time. It will be hard reset against the release each time.
+
+### Working Branches
+
+Branches should be taken out of `development`, prior to each pull request they should be *rebased* and *not `merged`*.
+
+- `f/a-very-good-name`: Should be used for a feature.
+- `b/a-very-good-name`: Should be used for a bug.
+- `c/a-very-good-name`: Should be used for a chore.
+- `t/a-very-good-name`: Should be used for adding tests.
+- `w/a-very-good-name`: Should be used for a work-in-progress branch.
+
+Other tips:
+
+1. No name more than 30 characters.
+2. Do not use `_` as a space, use `-` instead.
+3. Do not include merge commits, always rebase instead.
---
+++ scripts/commitFormat.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+
+const fs = require("fs")
+
+const validGitmoji = [
+  "art",
+  "zap",
+  "fire",
+  "bug",
+  "ambulance",
+  "sparkles",
+  "memo",
+  "rocket",
+  "lipstick",
+  "tada",
+  "white_check_mark",
+  "lock",
+  "apple",
+  "penguin",
+  "checkered_flag",
+  "robot",
+  "green_apple",
+  "bookmark",
+  "rotating_light",
+  "construction",
+  "green_heart",
+  "arrow_down",
+  "arrow_up",
+  "pushpin",
+  "construction_worker",
+  "chart_with_upwards_trend",
+  "recycle",
+  "heavy_minus_sign",
+  "whale",
+  "heavy_plus_sign",
+  "wrench",
+  "globe_with_meridians",
+  "pencil2",
+  "hankey",
+  "rewind",
+  "twisted_rightwards_arrows",
+  "package",
+  "alien",
+  "truck",
+  "page_facing_up",
+  "boom",
+  "bento",
+  "ok_hand",
+  "wheelchair",
+  "bulb",
+  "beers",
+  "speech_balloon",
+  "card_file_box",
+  "loud_sound",
+  "mute",
+  "busts_in_silhouette",
+  "children_crossing",
+  "building_construction",
+  "iphone",
+  "clown_face",
+  "egg",
+  "see_no_evil",
+  "camera_flash",
+]
+// Replace all underscores with escaped underscores
+.map(gitmoji => gitmoji.replace("_", "\\_"))
+// Add `:` to the ends of each Gitmoji to make them actual emoji's, also escaped
+.map(gitmoji => `\\:${gitmoji}\\:`)
+// Convert to a valid Regex group by joining with a pipe `|`
+.join("|")
+
+const requirements = [
+  [new RegExp(`^(${validGitmoji})+\\s.{0,49}\n`), "Title must be less than 50 characters. Not including Gitmoji"],
+  [new RegExp(`^(${validGitmoji})+`), "Title needs at least one valid Gitmoji: https://gitmoji.carloscuesta.me"],
+  [/^(\:([0-9]|[a-z]|[_])+\:)+\s/, "Title needs a space after all Gitmoji"],
+  [/^(\:([0-9]|[a-z]|[_])+\:)+\s[A-Z]/, "Title must begin with a capital letter."],
+  [/^.*[^\.]\n/, "Title must not have a period at the end."],
+]
+
+const formatProblem = (message, rule) => (message.match(rule[0]) ? null : rule[1])
+
+const formatProblems = (message, rules) =>
+  rules.map(requirement => formatProblem(message, requirement)).filter(error => Boolean(error))
+
+const validateCommitMessage = (message, rules) =>
+  formatProblems(message, rules)
+    .map(mes => console.error(mes))
+    .map(() => process.exit(1))
+
+const getCommitFile = () => {
+  const commitMessage = process.argv[2]
+  if (commitMessage) return commitMessage
+  console.error("Commit message file doesn't exist.")
+  process.exit(1)
+}
+
+fs.readFile(getCommitFile(), "utf8", (fileError, message) => {
+  if (fileError) {
+    console.error(fileError)
+    return process.exit(1)
+  }
+  validateCommitMessage(message, requirements)
+})
