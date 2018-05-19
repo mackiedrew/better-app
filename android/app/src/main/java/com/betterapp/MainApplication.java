7ea5234e6cbb0c3603809c492b64fe3e900d5cfd
Mackie Drew <mackiedrew@gmail.com>
2018-05-19 17:07:36 -0400

    :sparkles: React Native Navigation for Android

--- android/app/src/main/java/com/betterapp/MainApplication.java
+++ android/app/src/main/java/com/betterapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.betterapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -11,35 +12,26 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
   }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+      // eg. new VectorIconsPackage()
+    );
+  }
+
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+
+  public String getJSMainModuleName() {
+    return "index";
   }
 }
